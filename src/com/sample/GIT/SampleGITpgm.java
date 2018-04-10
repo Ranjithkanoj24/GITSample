07bf26f55eb5483bf00c663f1b5379fcbef31e5a
Ranjith Kumar B <ranjithkanoj24@gmail.com>
2018-04-10 11:57:59 +0530

    Update SampleGITpgm.java

--- src/com/sample/GIT/SampleGITpgm.java
+++ src/com/sample/GIT/SampleGITpgm.java
@@ -1,7 +1,7 @@
 package com.sample.GIT;
 
 public class SampleGITpgm {
-	//This is a sample commands
+	//This is a sample commit
 	public static void main(String[] args) {
 		System.out.println("This is a sample GIT Project");
 	}
